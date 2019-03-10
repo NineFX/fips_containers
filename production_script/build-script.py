8cd95faf3e0591d8d648135ee6dbce64118c1756
Ishrat Singh <ishratsingh@Ishrats-MacBook-Pro.local>
2019-03-09 19:19:21 -0500

    openssl-fips Dockerfile generation

--- production_script/build-script.py
+++ production_script/build-script.py
@@ -1,8 +1,8 @@
 import yaml
-from os import popen
+from os import popen, chdir
 
 # READ IN YAML FILE
-with open('fips-packages.yml', 'r') as yamlstream:
+with open('packages.yml', 'r') as yamlstream:
     try:
         raw = yaml.load(yamlstream)
     except yaml.YAMLError as e:
@@ -14,11 +14,35 @@ verify = lambda url, sha256: popen('curl -s ' + url + ' | shasum -a 256').read()
 # MASTER FOR-LOOP
 for package in raw:
     # SHASUM VERIFICATION STEP - PROGRAM EXITS IF A SHASUM CHECK FAILS
+    name = package.keys()[0]
     url = package[package.keys()[0]]['url']
     sha256 = package[package.keys()[0]]['sha256']
+    version = package[package.keys()[0]]['version']
     if(verify(url, sha256)):
-        print 'sha256 sum check failed for package ' + package.keys()[0] + ', exiting...'
+        print 'sha256 sum check failed for package ' + name + ', exiting...'
         break
     # ALPINE DOCKERFILE GENERATION STEP
-    with open(package.keys()[0] + '-fips-alpine.Dockerfile', 'w') as fstream:
-      fstream.write('FROM alpine:latest')
+    pkg_name = name + '-fips-alpine'
+    popen('mkdir ' + pkg_name)
+    chdir(pkg_name)
+    with open('Dockerfile', 'w') as dfstream:
+        dfstream.write    ('FROM alpine:latest AS build\n')
+        dfstream.write    ('WORKDIR /\n')
+        dfstream.write    ('RUN set -eux; \\\n')
+        dfstream.write    ('    apk add --no-cache \\\n')
+        dfstream.write    ('        ca-certificates \\\n')
+        dfstream.write    ('        curl \\\n')
+        dfstream.write    ('        gcc \\\n')
+        dfstream.write    ('        g++ \\\n')
+        dfstream.write    ('        make \\\n')
+        dfstream.write    ('        cmake \\\n')
+        dfstream.write    ('        git \\\n')
+        dfstream.write    ('        perl \\\n')
+        dfstream.write    ('        tar; \\\n')
+        dfstream.write    ('    curl -O ' + url + '; \\\n')
+        dfstream.write    ('    tar -zxvf ' + name + '-' + version + '.tar.gz; \\\n')
+        dfstream.write    ('    cd ' + name + '-' + version + '; \\\n')
+        if (name == 'openssl'):
+            dfstream.write('    ./config; \\\n')
+            dfstream.write('    make; \\\n')
+            dfstream.write('    make install')
